ed17914ffbbfe8bd2190996e596132c454cd9216
Joan González-Conde Cantero <Joan.Gonzalez-CondeCantero@vistaprint.com>
2022-02-03 12:16:19 +0100

    code prepared to create auxiliary RDS file to save config info necessary for adhoc OutputCollect creation

--- R/R/model.R
+++ R/R/model.R
@@ -956,6 +956,17 @@ robyn_run <- function(InputCollect,
       InputCollect
       ,paste0(plot_folder, "/", plot_folder_sub, "/InputCollect.rds")
     )
+    print("Saving auxiliary RDS with key config parameters to reproduce OutputCollect.RDS")
+    list_dummy <-list(
+      calibration_constraint = calibration_constraint,
+      intercept_sign = intercept_sign,
+      UI = UI
+      )
+    list.save(
+      InputCollect
+      ,paste0(plot_folder, "/", plot_folder_sub, "/auxiliaryConfig.rds")
+      )
+    
   }
   return(output)
 }
