e37ab7d22d952c4b9be7c4b418e3ede93574fe2f
Joan González-Conde Cantero <92848379+joangcc@users.noreply.github.com>
2023-06-16 10:34:34 +0200

    Debug: uncommeting print statemtn for coef(mod)

--- R/R/model.R
+++ R/R/model.R
@@ -1155,7 +1155,7 @@ model_refit <- function(x_train, y_train, x_val, y_val, x_test, y_test,
         #print(required_effect_attrib)
         # Check whether required direct effect within mod object is null. If that's the case, then penalize botht he moderator and the direct effect to 0.
         #print("printing value of coef(mod)[required_effect_attrib]")
-        #print(coef(mod)[required_effect_attrib])
+        print(coef(mod)[required_effect_attrib])
         coef(mod)[required_effect_attrib] = 0 # Test condition
         if (coef(mod)[required_effect_attrib] == 0){
           print("detected coef(mod)[required_effect_attrib] == 0")
