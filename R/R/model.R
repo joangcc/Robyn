8124604e80afcb1fcbf090d4e67fc9e1a506ffc8
Joan González-Conde Cantero <92848379+joangcc@users.noreply.github.com>
2023-06-16 09:34:43 +0200

    Debugging: commented required_effect_attribute_condition

--- R/R/model.R
+++ R/R/model.R
@@ -1155,14 +1155,14 @@ model_refit <- function(x_train, y_train, x_val, y_val, x_test, y_test,
         # Check whether required direct effect within mod object is null. If that's the case, then penalize botht he moderator and the direct effect to 0.
         #print("printing value of coef(mod)[required_effect_attrib]")
         print(coef(mod)[required_effect_attrib])
-        if (coef(mod)[required_effect_attrib] == 0){
-          print("detected coef(mod)[required_effect_attrib] == 0")
-          pos_penalty_direct = as.integer(which(sapply(names(lares::ohse(select(dt_window, -.data$dep_var))), function(x) required_effect_attrib %in% x)))
-          penalty.factor[pos_penalty_direct] = 0
-          mod_attrib = as.character(subset(moderator_dependencies, (required_effect_attribute == required_effect_attrib), select=c(moderator_attribute)))
-          pos_penalty_mod = as.integer(which(sapply(names(lares::ohse(select(dt_window, -.data$dep_var))), function(x) mod_attrib %in% x)))
-          penalty.factor[pos_penalty_mod] = 0                                                           
-        }
+        #if (coef(mod)[required_effect_attrib] == 0){
+          #print("detected coef(mod)[required_effect_attrib] == 0")
+          #pos_penalty_direct = as.integer(which(sapply(names(lares::ohse(select(dt_window, -.data$dep_var))), function(x) required_effect_attrib %in% x)))
+          #penalty.factor[pos_penalty_direct] = 0
+          #mod_attrib = as.character(subset(moderator_dependencies, (required_effect_attribute == required_effect_attrib), select=c(moderator_attribute)))
+          #pos_penalty_mod = as.integer(which(sapply(names(lares::ohse(select(dt_window, -.data$dep_var))), function(x) mod_attrib %in% x)))
+          #penalty.factor[pos_penalty_mod] = 0                                                           
+        #}
       }
       mod <- glmnet(
       x_train,
