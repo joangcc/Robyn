e077bfc3793820fd2b1e9b87e593d6a9f9dab59f
Joan González-Conde Cantero <92848379+joangcc@users.noreply.github.com>
2023-06-16 11:43:12 +0200

    Fix: added closing parenthesis

--- R/R/model.R
+++ R/R/model.R
@@ -1151,7 +1151,7 @@ model_refit <- function(x_train, y_train, x_val, y_val, x_test, y_test,
       print(paste("length of req_attrib_list is:", length(req_attrib_list)))
       print(seq_along(req_attrib_list))
       #for (required_effect_attrib in seq_along(req_attrib_list)){
-      for (i in 1:length(req_attrib_list){
+      for (i in 1:length(req_attrib_list)){
         required_effect_name = req_attrib_list[i]
         print(i)
         print("printing required_effect_name")
