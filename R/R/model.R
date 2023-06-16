b6750fbba0863b561607355cba1883a7d034c440
Joan González-Conde Cantero <92848379+joangcc@users.noreply.github.com>
2023-06-16 08:48:44 +0200

    print of coef(mod) value

--- R/R/model.R
+++ R/R/model.R
@@ -1154,7 +1154,7 @@ model_refit <- function(x_train, y_train, x_val, y_val, x_test, y_test,
         #print(required_effect_attrib)
         # Check whether required direct effect within mod object is null. If that's the case, then penalize botht he moderator and the direct effect to 0.
         #print("printing value of coef(mod)[required_effect_attrib]")
-        #print(coef(mod)[required_effect_attrib])
+        print(coef(mod)[required_effect_attrib])
         if (coef(mod)[required_effect_attrib] == 0){
           print("detected coef(mod)[required_effect_attrib] == 0")
           pos_penalty_direct = as.integer(which(sapply(names(lares::ohse(select(dt_window, -.data$dep_var))), function(x) required_effect_attrib %in% x)))
